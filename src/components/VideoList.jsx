c8277c1499f3d5a3850960bdec40b24254181f7e
mcooper92 <mcooper92@gmail.com>
2017-12-13 16:19:54 -0800

    Rendered exampleData everything & now working on click handlers

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,17 +1,17 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.videos.map((video) => {
+      return (<VideoListEntry video={video} />);
+    })}
+    
+    
   </div>
 );
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
-  videos: React.PropTypes.array.isRequired
+  videos: React.PropTypes.array.isRequired, 
 };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
